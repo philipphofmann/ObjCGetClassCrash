215c5eaf1a5f7fbd455eab4350f4f01bd7c8c660
Philipp Hofmann <philipp.hofmann@sentry.io>
2024-03-28 14:49:46 +0100

    Initial Commit

---
+++ ObjCGetClassCrash/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ObjCGetClassCrash
+//
+//  Created by Philipp Hofmann on 28.03.24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ ObjCGetClassCrash/ObjCGetClassCrashApp.swift
@@ -0,0 +1,17 @@
+//
+//  ObjCGetClassCrashApp.swift
+//  ObjCGetClassCrash
+//
+//  Created by Philipp Hofmann on 28.03.24.
+//
+
+import SwiftUI
+
+@main
+struct ObjCGetClassCrashApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
